1d4a75d687d7e55a83f1e252a5d22aa4bdafb417
punith <punith.bp@nextbrainitech.com>
2018-09-20 12:46:20 +0530

    initial heroku commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyABuV-9VLOJcQxPHozUqi_3YaTd0VzGiQs",
+    authDomain: "oshop-ac155.firebaseapp.com",
+    databaseURL: "https://oshop-ac155.firebaseio.com",
+    projectId: "oshop-ac155",
+    storageBucket: "oshop-ac155.appspot.com",
+    messagingSenderId: "112276734946" 
+  }
 };
