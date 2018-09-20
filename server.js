29fbe275025326ae35bd3ce9d189a73e06c72dcc
punith <punith.bp@nextbrainitech.com>
2018-09-20 15:53:03 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const path =require('path');
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/oshop'));
 
 // PathLocationStrategy
 
 app.get('/*', (req, res) =>{
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/oshop/index.html'));
 });
 
 console.log('Console Listening!');
+
+app.listen(process.env.PORT || 8080);
