32f355babd495cffde4353cfba2cf28dc7bb0e0c
punith <punith.bp@nextbrainitech.com>
2018-09-20 12:48:54 +0530

    initial heroku commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase:{
-    apiKey: "AIzaSyABuV-9VLOJcQxPHozUqi_3YaTd0VzGiQs",
-    authDomain: "oshop-ac155.firebaseapp.com",
-    databaseURL: "https://oshop-ac155.firebaseio.com",
-    projectId: "oshop-ac155",
-    storageBucket: "oshop-ac155.appspot.com",
-    messagingSenderId: "112276734946" 
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyABuV-9VLOJcQxPHozUqi_3YaTd0VzGiQs",
-    authDomain: "oshop-ac155.firebaseapp.com",
-    databaseURL: "https://oshop-ac155.firebaseio.com",
-    projectId: "oshop-ac155",
-    storageBucket: "oshop-ac155.appspot.com",
-    messagingSenderId: "112276734946"
-  },
-
+  production: false
 };
 
 /*
